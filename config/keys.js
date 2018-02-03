f09612c6e11ee4e909e0ca0f1edc849ef32dc1d4
Ashley <ashley.spellman@me.com>
2018-02-03 13:40:07 -0800

    prod and dev

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  googleClientID: "49499595988-ughehfarve6utdhir8pgg1e8r4j1nkko.apps.googleusercontent.com",
-  googleClientSecret: "fLOMS7psSv-t7_PyQzAMgeBK",
-  mongoURI: "mongodb://ashley:JB880240@ds121238.mlab.com:21238/meow-chat",
-  cookieKey: 'kljdkjhfjksdhajfke'
-}
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod');
+} else {
+  modult.exports = require('./dev');
+};
