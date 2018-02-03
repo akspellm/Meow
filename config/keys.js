6250a4da35111db38b434471d4acf4425d44c356
Ashley <ashley.spellman@me.com>
2018-02-03 12:47:41 -0800

    fuuuuck

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: "49499595988-ughehfarve6utdhir8pgg1e8r4j1nkko.apps.googleusercontent.com",
+  googleClientSecret: "fLOMS7psSv-t7_PyQzAMgeBK",
+  mongoURI: "mongodb://ashley:JB880240@ds121238.mlab.com:21238/meow-chat",
+  cookieKey: 'kljdkjhfjksdhajfke'
+}
