aa5bf5b2d6cffda6c27af742c9eb8bfebaee4231
Ashley <ashley.spellman@me.com>
2018-02-03 12:18:39 -0800

    heroku commit

--- client/src/components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Button.css";
-
-const Button = () =>
-  <div>
-    <button type="button" className="btn btn-primary center-block">Meow</button>
-    <p className="explain">Press and hold to capture your furry friends message</p>
-  </div>
-export default Button;
