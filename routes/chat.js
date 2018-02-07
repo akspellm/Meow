345f900e73814c7fb734629ce6882f34918bea4e
Ashley <ashley.spellman@me.com>
2018-02-06 19:48:05 -0800

    started refactor

--- routes/chat.js
+++ routes/chat.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 const Chat = mongoose.model('chat');
 
 module.exports = app => {
-  app.get('/api/chat', async (req, res) => {
-    const chat = await Chat.find({ "emotion": "friendly"});
-    console.log("hi" + chat)
-    res.send("hi" + chat);
+  app.get('/api/chat', requireLogin, async (req, res) => {
+    const newChat = await Chat.find({ "emotion": "friendly" });
+    res.send(newChat);
   })
 }
