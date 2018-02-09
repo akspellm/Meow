379bdccda45921f3ba80861850245a553edabc1a
Ashley Spellman <ashley.spellman@me.com>
2018-02-09 10:24:17 -0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -5,5 +5,9 @@ module.exports =
   "Scratch my head!",
   "Im hungry!",
   "Lets cuddle!",
-  "Bow before me human"
+  "Bow before me human",
+  "This is a catastrophe!",
+  "That's yours? No, mine.",
+  "Go Away! Don't leave me!",
+  "I want to go outside...Nevermind"
 ];
